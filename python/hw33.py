f2cf9248ed7c35ef218cb108f1febf40c035cd3f
jwatts31 <wattsjustin@gmail.com>
2017-01-29 14:21:13 -0500

    adding problem statement to the top of the files

--- python/hw33.py
+++ python/hw33.py
@@ -1,20 +1,31 @@
-#What is my grade
-score = raw_input("Enter in the score:")
+'''
+3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+    Score Grade
+    >= 0.9 A
+    >= 0.8 B
+    >= 0.7 C
+    >= 0.6 D
+    < 0.6 F
+    If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+'''
+
+score = raw_input("Enter a score betweem 0.0 and 1.0:")
+
 try:
     floatScore = float(score)
 except:
     print "Numeric Value was not entered, please enter numeric value"
     quit()
 
-if (floatScore >= 0.9) and (floatScore <= 1.0):
+if (0.9 <= floatScore <= 1.0):
     print "A"
-elif (floatScore >= 0.8) and (floatScore <= .89):
+elif (.80 <= floatScore <= .89):
     print "B"
-elif (floatScore >= 0.7) and (floatScore <= .79):
+elif (.70 <= floatScore <= .79):
     print "C"
-elif (floatScore >= 0.6) and (floatScore <= .69):
+elif (0.6 <= floatScore <= .69):
     print "D"
-elif (floatScore < 0.6) and (floatScore >= 0.0):
+elif (0.0 <= floatScore < 0.6):
     print "F"
 else:
     print "Score Range must be between 0.0 to 1.0"
--- python/hw51.py
+++ python/hw51.py
@@ -1,4 +1,11 @@
-#set variables
+'''
+5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+Once 'done' is entered, print out the largest and smallest of the numbers. 
+If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. 
+Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
+'''
+
+#initialize variables
 maxNum = None
 minNum = None
 numsEnter = None
@@ -16,5 +23,6 @@ while True:
         continue
     
     numList.append(intNumsEnter)
+
 print "Maximum is",max(numList)
 print "Minimum is",min(numList)
